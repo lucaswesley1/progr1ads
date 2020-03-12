efd69ad6e90034993dbbf69a9d4550b4afdee8bd
lucaswesley1 <lucasdutra@ads.fiponline.edu.br>
2020-03-11 21:05:54 -0300

    questao3

---
+++ questao3.py
@@ -0,0 +1,5 @@
+km_inicial = int(input("informe km inicial: "))
+km_final = int(input("informe km final: "))
+qtda_litros = float(input("quantos litros foram gastos no percusso: "))
+media = (km_final - km_inicial) * qtda_litros
+print("a media de combustivel foi de", media)
