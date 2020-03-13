9ffe6f4aa4c4d0050d3ee461f072a1904cbacd3f
lucaswesley1 <lucasdutra@ads.fiponline.edu.br>
2020-03-13 19:30:53 -0300

    questao6

---
+++ questao6.py
@@ -0,0 +1,14 @@
+comodo1_larg = int(input("informe a largura do comodo 1: "))
+comodo1_comp = int(input("informe o comprimento do comodo 1: "))
+comodo2_larg = int(input("informe a largura do comodo 2: "))
+comodo2_comp = int(input("informe o comprimento do comodo 2: "))
+comodo3_larg = int(input("informe a largura do comodo 3: "))
+comodo3_comp = int(input("informe o comprimento do comodo 3: "))
+comodo4_larg = int(input("informe a largura do comodo 4: "))
+comodo4_comp = int(input("informe o comprimento do comodo 4: "))
+area_comodo1 = comodo1_comp * comodo1_larg
+area_comodo2 = comodo2_comp * comodo2_larg
+area_comodo3 = comodo3_comp * comodo3_larg
+area_comodo4 = comodo4_comp * comodo4_larg
+area_total = area_comodo1 * area_comodo2 * area_comodo3 * area_comodo4
+print(area_total)
