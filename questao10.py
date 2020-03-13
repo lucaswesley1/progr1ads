8a073e9dd4662c014de33bc43cf63f65761008f9
lucaswesley1 <lucasdutra@ads.fiponline.edu.br>
2020-03-13 19:59:29 -0300

    questao10

---
+++ questao10.py
@@ -0,0 +1,4 @@
+salario_atual = float(input("informe o salario inicial: "))
+reajuste = float(input("informe o valor do reajuste: "))
+valor = salario_atual + reajuste
+print("o novo valor do salario foi de %.2f" % (valor))
